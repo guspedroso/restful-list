c1a2411cde6cf43cef638aa5023f113307cd11ec
Gustavo Pedroso <gus@pedroso.com>
2019-08-04 20:59:49 -0500

    adding enzyme for testing

--- src/components/Action/Action.test.js
+++ src/components/Action/Action.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Action from './Action';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, mount, render } from 'enzyme';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
+    const div = document.createElement('div');
+    ReactDOM.render(
         <Action />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('toggling the remove button works', () => {
+    const wrapper = shallow(<Action />);
+    expect(wrapper.state('removeView')).toBe(false);
 });
