0b320bdaa5f600dab6657a4f634b172e43c190dd
Gustavo Pedroso <gus@pedroso.com>
2019-08-06 23:35:34 -0500

    test for filter

---
+++ src/components/Filter/Filter.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Filter from './Filter';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from 'enzyme';
+import { ProviderWrapper } from '../../getProvider';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <ProviderWrapper>
+            <Filter />
+        </ProviderWrapper>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Snapshot works', () => {
+    let component = renderer.create(
+        <ProviderWrapper>
+            <Filter />
+        </ProviderWrapper>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
